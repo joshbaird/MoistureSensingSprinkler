a855819fbc5569d62a0956210a01c1f12ee72f67
Joshua Baird <JBaird@Alpine-la.com>
2015-04-27 07:32:15 -0700

    adding python script to change sensor address. use with syntax:
    python changeAddress.py fromAddress toAddress
    
    ex:
    python changeAddress.py 20 40

---
+++ python_scripts/changeAddress.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from chirp import Chirp
+import sys
+from time import sleep
+
+fromAddr = str(sys.arg[1]);
+toAddr = str(sys.arg[2]);
+print "Changing address from: " fromAddrm + ", to: " + toAddr; 
+
+chirp = Chirp(1,int(fromAddr,16));
+
+# change address by writting to register 1 then writing new value follow by a reset.
+# 1 = register 1, set address
+# 6 = reset
+# 7 = get version
+print "writing to reg 1";
+chirp.write(1);
+sleep(0.5);
+print "writing new address: " + toAddr;
+chrip.write(int(toAddr,16));
+sleep(0.5);
+print "resetting unit";
+chirp.write(6);
+sleep(6);
+print "testing new address..."
+chirp = Chirp(1,int(toAddr,16));
+print "read version from new Address: " + str(chirp.get_reg(7));
