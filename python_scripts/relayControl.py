7985ab6334a8bbb8071f5c23565ef92c7cdfb265
Josh <josh@Android.local>
2015-04-30 21:03:01 -0700

    fixed relay control to turn on when values drop below, and off when values go above max if it is already on.

--- python_scripts/relayControl.py
+++ python_scripts/relayControl.py
@@ -8,6 +8,7 @@ GPIO.setmode(GPIO.BOARD);
 
 # global exit flag
 exitFlag = False;
+sensorPinRunningFlags = {};
 ################### FUNCTIONS###############
 # GET port settings and sensor data from REST server
 def getPortSettings(url):
@@ -16,7 +17,10 @@ def getPortSettings(url):
 
 # Check to turn on relay for all sensors
 def checkSettings(port):
-	return checkTimeSettings(port) and checkMoistureSettings(port) and checkTempSettings(port) and checkLightSettings(port) and checkWeatherSettings(port);
+	return (checkTimeSettings(port) andcheckMoistureSettings(port, sensorPinRunningFlags[port["pinId"]]) and 
+		   	checkTempSettings(port, sensorPinRunningFlags[port["pinId"]]) and 
+		   	checkLightSettings(port, sensorPinRunningFlags[port["pinId"]]) and 
+		   	checkWeatherSettings(port, sensorPinRunningFlags[port["pinId"]]));
 
 def checkTimeSettings(port):
 	print "time now: " + str(time.time());
@@ -24,24 +28,23 @@ def checkTimeSettings(port):
 	print "time off: " + str(float(port["turnOffTime"])/1000);
 	return float(port["turnOnTime"])/1000 < time.time() and float(port["turnOffTime"])/1000 > time.time();
 
-def checkMoistureSettings(port):
+def checkMoistureSettings(port, checkforOn):
 	print "moisture: ";
 	currentVal =  port["moistureHistory"][len(port["moistureHistory"]) - 1]["value"]
 	print currentVal;
-	return int(port["turnOnMoisture"]) < int(currentVal) and int(port["turnOffMoisture"]) > int(currentVal);
+	return (checkforOn) ? int(port["turnOnMoisture"]) > int(currentVal) : int(port["turnOffMoisture"]) > int(currentVal);
 
-def checkTempSettings(port):
+def checkTempSettings(port, checkforOn):
 	print "temp: ";
 	currentVal =  port["tempHistory"][len(port["tempHistory"]) - 1]["value"];
 	print currentVal;
+	return (checkforOn) ?  int(port["turnOnTemp"]) > int(currentVal) : int(port["turnOffTemp"]) > int(currentVal);
 
-	return int(port["turnOnTemp"]) < int(currentVal) and int(port["turnOffTemp"]) > int(currentVal);
-
-def checkLightSettings(port):
+def checkLightSettings(port, checkforOn):
 	print "light: ";
 	currentVal =  port["lightHistory"][len(port["lightHistory"]) - 1]["value"];
 	print currentVal;
-	return int(port["turnOnLight"]) < int(currentVal) and int(port["turnOffLight"]) > int(currentVal);
+	return (checkforOn) ?  int(port["turnOnLight"]) > int(currentVal) : int(port["turnOffLight"]) > int(currentVal);
 
 # for now return true. will try and implement later.
 def checkWeatherSettings(port):
@@ -50,6 +53,7 @@ def checkWeatherSettings(port):
 # Turn On Relay by PORT
 def turnOnRelay(port):
 	print "turning pin: " + str(port["pinId"]) + " on...";
+	sensorPinRunningFlags[port["pinId"]] = True;
 	GPIO.setup(int(port["pinId"]), GPIO.OUT);
 	GPIO.output(int(port["pinId"]), False);
 	return;
@@ -57,6 +61,7 @@ def turnOnRelay(port):
 # Turn Off Relay by PORT
 def turnOffRelay(port):
 	print "turning pin: " + str(port["pinId"]) + " off...";
+	sensorPinRunningFlags[port["pinId"]] = False;
 	GPIO.setup(int(port["pinId"]), GPIO.OUT);
 	GPIO.output(int(port["pinId"]), True);
 	return;
@@ -73,6 +78,10 @@ def checkForExit():
 while (~checkForExit()):
 	values = getPortSettings('http://localhost:3000/sensors/sensorlist');
 	for value in values:
+		# make sure key exists... if not set set port to stable state and continue.
+		if (~sensorPinRunningFlags.has_key(value['pinId'])):
+			turnOffRelay(value);
+			
 		if (checkSettings(value)):
 			turnOnRelay(value);
 		else:
