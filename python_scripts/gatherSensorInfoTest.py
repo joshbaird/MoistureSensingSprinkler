c528c5efa8a31dc35182bd94731527c9e75ed46d
Joshua Baird <JBaird@Alpine-la.com>
2015-05-08 08:18:29 -0700

    - adding some test scripts

---
+++ python_scripts/gatherSensorInfoTest.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from chirp import Chirp
+import time
+from time import sleep
+import requests
+import json
+import sys
+
+# global exit flag
+exitFlag = False;
+################# Functions #####################
+# Scan I2C Bus return list of address 0-127
+def getSensorAddresses():
+	response = requests.get('http://localhost:3000/sensors/sensorlist');
+	items =  response.json;
+	print items;
+	addresses = []
+	for item in items:
+		if('sensorType' in item):
+			if(item['sensorType'] == "i2c"):
+				addresses.append(item);
+	print addresses;
+	return addresses;
+
+# Read Address and POST data to server
+def ReadAddressAndPOST(sensor):
+	addr = sensor["sensorId"];
+	print addr;
+	#chirp = Chirp(1,int(addr,16));
+
+	# read moisture
+	currentValMoisture = sys.argv[0];#chirp.cap_sense();
+	print currentValMoisture;
+	if("moistureHistory" in sensor):
+		historyM = sensor["moistureHistory"];
+	else:
+		historyM = [];
+	moisture = {};
+	moisture["date"] = str(time.time()), 
+	moisture["value"] = str(currentValMoisture)
+	historyM.append(moisture);
+	print historyM;
+
+	#read light
+	currentValLight = sys.argv[1];#chirp.cap_sense();
+	print currentValLight;
+	if("lightHistory" in sensor):
+		historyL = sensor["lightHistory"];
+	else:
+		historyL = [];
+	light = {"date" : str(time.time()), "value" : str(currentValLight)};
+	historyL.append(light);
+	print historyL;
+
+	#read temp
+	currentValTemp = sys.argv[2]; #chirp.cap_sense();
+	print currentValTemp;
+	if("tempHistory" in sensor):
+		historyT = sensor["tempHistory"];
+	else:
+		historyT = [];
+	temp = {"date" : str(time.time()), "value" : str(currentValTemp)};
+	historyT.append(temp);
+	print historyT;
+	headers = {'content-type': 'application/json'};
+	#update all data fields
+	js = {
+		"sensorId" : addr,
+		"sensorType" : sensor["sensorType"],
+        "pinId" : sensor["pinId"],
+        "turnOnTime" : sensor["turnOnTime"],
+        "turnOffTime" : sensor["turnOffTime"],
+	    "turnOnMoisture" : sensor["turnOnMoisture"],
+        "turnOffMoisture" : sensor["turnOffMoisture"],
+		"moistureHistory" : historyM,
+		"turnOnLight" : sensor["turnOnLight"],
+        "turnOffLight" : sensor["turnOffLight"],
+		"lightHistory" : historyL,
+		"turnOnTemp" : sensor["turnOnTemp"],
+        "turnOffTemp" : sensor["turnOffTemp"],
+		"tempHistory" : historyT
+       	};
+	requests.post('http://localhost:3000/sensors/updatesensor', data=json.dumps(js), headers=headers);
+
+# check exitflag
+def checkForExit():
+	return exitFlag;
+
+################# main loop #####################
+sensors = getSensorAddresses();
+maxindex = len(sensors);
+index = 0;
+while (~checkForExit()):
+	sleep(1);
+	if(index >= maxindex):
+		sensors = getSensorAddresses();
+		maxindex = len(sensors);
+		index = 0;
+	else:
+		ReadAddressAndPOST(sensors[index]);
+		index+=1;
---
+++ python_scripts/relayControlTest.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+#import RPi.GPIO as GPIO
+import time
+from time import sleep
+import requests
+#GPIO.setmode(GPIO.BOARD);
+
+# global exit flag
+exitFlag = False;
+sensorPinRunningFlags = {};
+################### FUNCTIONS###############
+# GET port settings and sensor data from REST server
+def getPortSettings(url):
+	r = requests.get(url);
+	return r.json;
+
+# Check to turn on relay for all sensors
+def checkSettings(port):
+	return (checkTimeSettings(port) and 
+			checkMoistureSettings(port, sensorPinRunningFlags[port["pinId"]]) and 
+		   	checkTempSettings(port) and 
+		   	checkLightSettings(port) and 
+		   	checkWeatherSettings(port);
+
+# check to make sure time falls between on and off
+def checkTimeSettings(port):
+	print "time now: " + str(time.time());
+	print "time on:  " + str(float(port["turnOnTime"])/1000);
+	print "time off: " + str(float(port["turnOffTime"])/1000);
+	return float(port["turnOnTime"])/1000 < time.time() and float(port["turnOffTime"])/1000 > time.time();
+
+# Moisture turns it on or off based on if its on or off
+def checkMoistureSettings(port, runningFlag):
+	print "moisture: ";
+	currentVal =  port["moistureHistory"][len(port["moistureHistory"]) - 1]["value"]
+	print currentVal;
+	return (runningFlag) ? int(port["turnOffMoisture"]) < int(currentVal) : int(port["turnOnMoisture"]) > int(currentVal);
+
+# check to make sure temp falls between the on and off
+def checkTempSettings(port):
+	print "temp: ";
+	currentVal =  port["tempHistory"][len(port["tempHistory"]) - 1]["value"];
+	print currentVal;
+	return int(port["turnOnTemp"]) < int(currentVal) and int(currentVal) < int(port["turnOffTemp"]);
+# check to make light falls between the on and off
+def checkLightSettings(port):
+	print "light: ";
+	currentVal =  port["lightHistory"][len(port["lightHistory"]) - 1]["value"];
+	print currentVal;
+	return int(port["turnOnLight"]) < int(currentVal) and int(currentVal) < int(port["turnOffLight"]) ;
+
+# for now return true. will try and implement later.
+def checkWeatherSettings(port):
+	return True;
+
+# Turn On Relay by PORT
+def turnOnRelay(port):
+	print "turning pin: " + str(port["pinId"]) + " on...";
+	sensorPinRunningFlags[port["pinId"]] = True;
+#	GPIO.setup(int(port["pinId"]), GPIO.OUT);
+#	GPIO.output(int(port["pinId"]), False);
+	return;
+
+# Turn Off Relay by PORT
+def turnOffRelay(port):
+	print "turning pin: " + str(port["pinId"]) + " off...";
+	sensorPinRunningFlags[port["pinId"]] = False;
+#	GPIO.setup(int(port["pinId"]), GPIO.OUT);
+#	GPIO.output(int(port["pinId"]), True);
+	return;
+# Delay
+def delay(seconds):
+	sleep(seconds);
+	return;
+	
+# Checkforexit
+def checkForExit():
+	return exitFlag;
+
+################## Main loop ###################
+while (~checkForExit()):
+	values = getPortSettings('http://localhost:3000/sensors/sensorlist');
+	for value in values:
+		# make sure key exists... if not set set port to stable state and continue.
+		if (~sensorPinRunningFlags.has_key(value['pinId'])):
+			turnOffRelay(value);
+			
+		if (checkSettings(value)):
+			turnOnRelay(value);
+		else:
+			turnOffRelay(value);
+	delay(5);
